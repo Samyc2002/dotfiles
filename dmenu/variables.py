3ac58b62e49c7daf871db5f4c6358fc8ad8f919e
Samyc2002 <2000200402@iitdh.ac.in>
2022-07-22 17:18:25 +0530

    Just made lvim as default

--- dmenu/variables.py
+++ dmenu/variables.py
@@ -14,7 +14,7 @@ def dmenuTall(command: string, lines: numbers, accent: string, prompt: string) -
         return (f"{command} -i -l {lines} -fn Poppins-10 -nb \#282c34 -nf \#bbc2cf -sb \{accent} -sf \#282c34 -W 1900.8 -h 30 -X 9.6 -Y 5.4 -p {prompt}")
     
 code = "code"
-vim = "alacritty -e nvim"
+vim = "alacritty -e lvim"
 emacs = "emacsclient -c -a emacs"
 atom = "atom"
 
